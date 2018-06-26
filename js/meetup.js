f77731d122101238fabea0d94e5d4a64426569c1
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-06-26 17:58:33 +0200

    refactor: remove dynamic html for meetup section

--- js/meetup.js
+++ js/meetup.js
@@ -5,37 +5,9 @@
  */
 function callbackMeetup(data) {
   if (!!data && !!data.results && data.results.length > 0) {
-    var htmlTemplate =
-      '<div class="container">\
-      <div class="row">\
-        <div class="col-lg-8 mx-auto text-center">\
-          <h2 class="section-heading text-white">Prochain Meetup</h2>\
-          <hr class="light my-4">\
-          <div id="details">\
-            <p class="mb-4">\
-              <div class="row">\
-                <span class="icon col-1 fa fa-lg fa-calendar-alt"></span>\
-                <span class="col-11" id="next_meetup_date"></span>\
-              </div>\
-              <div class="row">\
-                <span class="icon col-1 fas fa-lg fa-map-marker-alt"></span>\
-                <span class="col-11" id="next_meetup_location"></span>\
-              </div>\
-              <div>\
-                <h3 id="next_meetup_name"></h3>\
-                <span id="next_meetup_description"></span>\
-              </div>\
-            </p>\
-          </div>\
-          <a class="btn btn-light btn-xl" id="next_meetup_url" href="" target="_blank">Je m\'inscris</a>\
-        </div>\
-      </div>\
-    </div>';
-
     var next_event = data.results[0];
     var oMeetupDiv = document.getElementById("meetup");
 
-    oMeetupDiv.innerHTML = htmlTemplate;
     document.getElementById("next_meetup_date").innerText = new Date(
       next_event.time
     ).toLocaleDateString();
