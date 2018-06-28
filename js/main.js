8ef0327c022f611a39ddb1bd40d58f224c0acac4
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-06-28 17:29:28 +0200

    feat: add link to virtual wallet

--- js/main.js
+++ js/main.js
@@ -23,6 +23,8 @@
     }
   });
 
+  $('[data-toggle="tooltip"]').tooltip();
+
   // Closes responsive menu when a scroll trigger link is clicked
   $(".js-scroll-trigger").click(function() {
     $(".navbar-collapse").collapse("hide");
