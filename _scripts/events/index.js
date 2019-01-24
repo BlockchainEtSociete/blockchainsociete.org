51f48b3757720c54a133c4f9e8aa9669855ca9c7
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-01-24 22:06:21 +0100

    refactor: algo recup des events depuis meetup

--- _scripts/events/index.js
+++ _scripts/events/index.js
@@ -6,66 +6,69 @@ const EVENTS_URL="https://api.meetup.com/2/events?&sign=true&photo-host=public&g
 
 const OUTPUT_FILE_PATH=path.join(__dirname,"hugo/data/events.json");
 
-async function buildResponse(response) {
-    return new Promise((resolve, reject) => {
-        let rawData = '';
-        response.on('data', (chunk) => { rawData += chunk; });
-        response.on('end', () => {
-            try {
-                resolve(rawData);
-            } catch (e) {
-                reject("Erreur lors de la récupération des données dans la réponse");
-            }
-        });
-    });
-}
-
-function parseResponse(callback){
-    return async (response)=>{
-        const { statusCode } = response;
-
-        if (statusCode === 200) {
-            console.log("Récupération des events passés OK");
-            callback(JSON.parse(await buildResponse(response)).results);
-            
-        } else {
-            console.log("Erreur lors de la récupération des events passés");
-            console.log(`Code retour : ${statusCode}`);
-            console.log(`Response : ${await buildResponse(response)}`);
-            process.exit(1);
-        }
-        response.resume();
-    }
-}
-
-function parseDate(date){
+function formatDate(date){
     let dateObj = new Date(date);
     let offset = dateObj.getTimezoneOffset() * 60 * 1000;
-    let local = dateObj.getTime() - offset;
-    return new Date(local).toISOString().slice(0,19);
+    let locale = dateObj.getTime() - offset;
+    let localeDate = new Date(locale);
+    let month = localeDate.getMonth() + 1;
+    month = month > 9 ? month : '0'+month;
+    return localeDate.getDate()+'/'+month+'/'+localeDate.getFullYear();
 }
 
+function formatTime(date,duration){
+    let locale = 'fr-FR';
+    let timeOptions = {hour:'2-digit', minute:'2-digit'}
+    let startTime = new Date(date).toLocaleTimeString(locale,timeOptions);
+    let endTime = new Date(date+duration).toLocaleTimeString(locale,timeOptions);
+    return startTime + ' - ' + endTime;
+}
+
+function formatVenue(venue){
+    return venue ? venue.name + ", " + venue.address_1 + " - " + venue.city : "Nantes";
+}
+
+function fetch(url) {
+    return new Promise((resolve, reject) => {
+        const request = https.get(url, (response) => {
+            if (response.statusCode < 200 || response.statusCode > 299) {
+                reject(new Error('Failed to load page, status code: ' + response.statusCode));
+            }
+            // temporary data holder
+            const body = [];
+            // on every content chunk, push it to the data array
+            response.on('data', (chunk) => body.push(chunk));
+            // we are done, resolve promise with those joined chunks
+            response.on('end', () => resolve(body.join('')));
+        });
+        // handle connection errors of the request
+        request.on('error', (err) => reject(err))
+    })
+};
+      
 function buildEventFile(events){
 
     let output = events.map(event => {
-        let venue = event.venue ? {
-            name: event.venue.name,
-            address_1: event.venue.address_1,
-            city: event.venue.city
-        } : {city:"NANTES"}
         return {
             name: event.name,
-            date: parseDate(event.time),
-            duration: events.duration || 10800000,
-            venue: venue,
-            event_url: event.venue_url
+            date: formatDate(event.time),
+            time: formatTime(event.time,event.duration || 10800000),
+            venue: formatVenue(event.venue),
+            url: event.event_url
         }
     });
 
     fs.writeFileSync(OUTPUT_FILE_PATH,JSON.stringify(output));
-    console.log("Terminé avec succès");
+    
 }
 
-// Récupération des évènements passés et futurs
-console.log("Récupération des events passés...");
-https.get(EVENTS_URL, parseResponse(buildEventFile));
+console.log("Récupération des évènements depuis api.meetup.com");
+
+fetch(EVENTS_URL)
+    .then(response => JSON.parse(response).results)
+    .then(buildEventFile)
+    .then(_=>console.log("Terminé avec succès"))
+    .catch((error)=>{
+        console.error(error);
+        process.exit(1);
+    })
