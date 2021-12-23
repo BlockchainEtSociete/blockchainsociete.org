5f7365dde6e483fb8ef8cbc5b3114a5101e67caf
NICOLAS GUILLAUME <guin56@gmail.com>
2021-12-23 17:20:35 +0100

    feat: improve homepage intro

---
+++ themes/custom-blist/assets/css/tailwind.config.js
@@ -0,0 +1,113 @@
+const themeDir = __dirname + "/../../";
+
+module.exports = {
+  purge: {
+    enabled: process.env.HUGO_ENVIRONMENT === "production",
+    content: [
+      themeDir + "layouts/**/*.html",
+      themeDir + "content/**/*.html",
+      "layouts/**/*.html",
+      "config.toml",
+      "content/**/*.html",
+      "assets/js/search.js",
+      "exampleSite/layouts/**/*.html",
+      "exampleSite/config.toml",
+      "exampleSite/content/**/*.html",
+    ],
+  },
+  darkMode: "class",
+  theme: {
+    extend: {
+      width: {
+        160: "40rem",
+      },
+      height: {
+        120: "30rem",
+      },
+      maxWidth: {
+        xxs: "9rem",
+        xs: "20rem",
+        sm: "24rem",
+      },
+      maxHeight: {
+        56: "14rem",
+      },
+      colors: {
+        black: {
+          400: "#363636",
+          700: "#101010",
+          900: "#000000",
+        },
+      },
+      typography: (theme) => ({
+        dark: {
+          css: [
+            {
+              color: theme("colors.gray.400"),
+              '[class~="lead"]': {
+                color: theme("colors.gray.300"),
+              },
+              a: {
+                color: theme("colors.white"),
+              },
+              strong: {
+                color: theme("colors.white"),
+              },
+              "ol > li::before": {
+                color: theme("colors.gray.400"),
+              },
+              "ul > li::before": {
+                backgroundColor: theme("colors.gray.600"),
+              },
+              hr: {
+                borderColor: theme("colors.gray.200"),
+              },
+              blockquote: {
+                color: theme("colors.gray.200"),
+                borderLeftColor: theme("colors.gray.600"),
+              },
+              h1: {
+                color: theme("colors.white"),
+              },
+              h2: {
+                color: theme("colors.white"),
+              },
+              h3: {
+                color: theme("colors.white"),
+              },
+              h4: {
+                color: theme("colors.white"),
+              },
+              "figure figcaption": {
+                color: theme("colors.gray.400"),
+              },
+              code: {
+                color: theme("colors.white"),
+              },
+              "a code": {
+                color: theme("colors.white"),
+              },
+              pre: {
+                color: theme("colors.gray.200"),
+                backgroundColor: theme("colors.gray.800"),
+              },
+              thead: {
+                color: theme("colors.white"),
+                borderBottomColor: theme("colors.gray.400"),
+              },
+              "tbody tr": {
+                borderBottomColor: theme("colors.gray.600"),
+              },
+            },
+          ],
+        },
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      typography: ["dark"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
