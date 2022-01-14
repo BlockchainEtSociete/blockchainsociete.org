8b53c9c7fb1362fc8ba7a2931e3847a514628761
NICOLAS GUILLAUME <guin56@gmail.com>
2022-01-14 18:16:32 +0100

    chores: use meetup graphql endpoint

--- scripts/fetch-events/constants.js
+++ scripts/fetch-events/constants.js
@@ -1,13 +1,45 @@
 const path = require("path");
 
-/**
- * @see https://www.meetup.com/meetup_api/docs/:urlname/events/#list
- */
-const EVENTS_URL =
-  "https://api.meetup.com/Blockchain-Societe-Nantes/events?fields=featured_photo&status=past,upcoming&page=100&desc=true";
 const EVENT_PAGES_PATH = path.join(__dirname, "../../content/fr/events");
 
+const MEETUP_ENDPOINT_GRAPHQL = "https://api.meetup.com/gql";
+const MEETUP_FETCH_EVENTS_GRAPHQL_QUERY = `
+query {
+  groupByUrlname (urlname:"Blockchain-Societe") {
+        upcomingEvents(input:{first:100}) {
+          edges {
+              node{
+                  title,
+                  shortUrl,
+                  description,
+                  dateTime,
+                  duration,
+                  venue { name, address, city },
+                  topics{ edges{ node{ name } } },
+                  imageUrl,
+              }
+          }
+      },
+      pastEvents(input:{first:100}) {
+          edges{
+              node{
+                  title,
+                  shortUrl,
+                  description,
+                  dateTime,
+                  duration,
+                  venue { name, address, city },
+                  topics{ edges{ node{ name } } },
+                  imageUrl,
+              }
+          }
+      },
+  }
+}
+`;
+
 module.exports = {
-  EVENTS_URL,
   EVENT_PAGES_PATH,
+  MEETUP_ENDPOINT_GRAPHQL,
+  MEETUP_FETCH_EVENTS_GRAPHQL_QUERY,
 };
